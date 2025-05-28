6638c2362f3c6953e0f312ae78b9bfda8b108943
divyasri mygapula@LAPTOP-9JOAST2M <dineshmygapula.mca@gmail.com>
2025-05-28 14:20:17 +0530

    runners order

--- SpringBootCore/bootIOCProject12-Runners/src/main/java/com/nt/runners/apprunnerOne.java
+++ SpringBootCore/bootIOCProject12-Runners/src/main/java/com/nt/runners/apprunnerOne.java
@@ -3,18 +3,25 @@ package com.nt.runners;
 import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
-public class apprunnerOne implements CommandLineRunner {
+//@Order(1)
+public class apprunnerOne implements CommandLineRunner,Ordered {
 
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("apprunnerOne.run()::start");
 		System.out.println("=======Runner Class One (CommandLineRunner)===========");
-		System.out.println("The Command Line Arguments are "+Arrays.toString(args));
+		System.out.println("The Command Line Arguments are " + Arrays.toString(args));
 
 		System.out.println("apprunnerOne.run()::end");
 	}
-
+	
+	@Override
+	public int getOrder() {
+		return 3;
+	}
 }
--- SpringBootCore/bootIOCProject12-Runners/src/main/java/com/nt/runners/apprunnerTwo.java
+++ SpringBootCore/bootIOCProject12-Runners/src/main/java/com/nt/runners/apprunnerTwo.java
@@ -4,10 +4,13 @@ import java.util.Set;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
-public class apprunnerTwo implements ApplicationRunner {
+//@Order(2)
+public class apprunnerTwo implements ApplicationRunner,Ordered {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
@@ -17,11 +20,16 @@ public class apprunnerTwo implements ApplicationRunner {
 		System.out.println("Non Option arguments" + args.getNonOptionArgs());
 
 		Set<String> setdata = args.getOptionNames();
-		
-		System.out.println("Option Arguments"+setdata);
-		
+
+		System.out.println("Option Arguments" + setdata);
+
+		setdata.forEach((name) -> System.out.println(name + " " + args.getOptionValues(name)));
 
 		System.out.println("apprunnerTwo.run():: end");
 	}
-
+	
+	@Override
+	public int getOrder() {
+		return 1;
+	}
 }
