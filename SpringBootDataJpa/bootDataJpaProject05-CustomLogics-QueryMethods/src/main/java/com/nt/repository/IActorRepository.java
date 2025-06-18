fe9bb2aee72690f243fa0aa85f4c6b875ceece6d
divyasri mygapula@LAPTOP-9JOAST2M <dineshmygapula.mca@gmail.com>
2025-06-18 15:06:47 +0530

    query methods and native sql

--- SpringBootDataJpa/bootDataJpaProject05-CustomLogics-QueryMethods/src/main/java/com/nt/repository/IActorRepository.java
+++ SpringBootDataJpa/bootDataJpaProject05-CustomLogics-QueryMethods/src/main/java/com/nt/repository/IActorRepository.java
@@ -1,10 +1,13 @@
 package com.nt.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nt.entity.Actor;
 
@@ -32,7 +35,27 @@ public interface IActorRepository extends JpaRepository<Actor, Integer> {
 
 	@Query("select aid ,name ,contactnumber from Actor where contactnumber=:mobile")
 	public Object showActorDataByMobileNumber(Long mobile);
-	
+
 	@Query("select name from Actor where contactnumber=:mobile")
 	public String showActorNameByMobileNumber(Long mobile);
+
+	@Query("select count(distinct name) from Actor")
+	public long countUniqueActors();
+
+	@Query("select count(*),max(fee),min(fee),avg(fee),sum(fee) from Actor")
+	public Object showActorsAggregateDate();
+
+	@Query("update Actor set fee=fee+(fee* :hikepercent/100.00) where category=:cat")
+	@Modifying
+	@Transactional
+	public int hikeActorFeeByCategory(String cat, double hikepercent);
+
+	@Query("delete from Actor where addr=:city")
+	@Modifying
+	@Transactional
+	public int removeActorsByAddr(String city);
+	
+	
+	@Query(value="Select sysdate from dual",nativeQuery=true)
+	public LocalDateTime showsystemDateTime();
 }
--- SpringBootDataJpa/bootDataJpaProject05-CustomLogics-QueryMethods/src/main/java/com/nt/runners/QueryMethodsTestRunner.java
+++ SpringBootDataJpa/bootDataJpaProject05-CustomLogics-QueryMethods/src/main/java/com/nt/runners/QueryMethodsTestRunner.java
@@ -1,13 +1,11 @@
 package com.nt.runners;
 
-import java.util.Arrays;
-import java.util.Optional;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.nt.entity.Actor;
 import com.nt.repository.IActorRepository;
 
 @Component
@@ -19,67 +17,108 @@ public class QueryMethodsTestRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+//		try {
+//			actorrepo.showActorsByFeeRange(1000.0, 1500.0).forEach(System.out::println);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("===========================================================================");
+//		try {
+//			actorrepo.showActorsByCategory("Drama", "Horror", "comdey").forEach(System.out::println);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		System.out.println("===========================================================================");
+//
+//		try {
+//			actorrepo.showActorsByAddr("Mumbai").forEach(row -> System.out.println(Arrays.toString(row)));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		System.out.println("===========================================================================");
+//
+//		try {
+//			actorrepo.showActorsNameByFeeRange(1000, 1500).forEach(System.out::println);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		System.out.println("=========================");
+//
+//		try {
+//			Optional<Actor> opt = actorrepo.showActorByMobileNumber(9876543237L);
+//			if (opt.isEmpty()) {
+//				System.out.println("Actor not found");
+//			} else {
+//				System.out.println(opt.get());
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		System.out.println("=========================");
+//		try {
+//			Object obj = actorrepo.showActorDataByMobileNumber(9876543237L);
+//
+//			Object row[] = (Object[]) obj;
+//
+//			System.out.println("Actor id " + row[0]);
+//			System.out.println("Actor name " + row[1]);
+//			System.out.println("Actor contact number " + row[2]);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("=========================");
+//
+//		try {
+//			String name = actorrepo.showActorNameByMobileNumber(9876543237L);
+//			System.out.println("Actor name " + name);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//		try {
+//			Long uniquecount =actorrepo.countUniqueActors();
+//			System.out.println("Unique Actors Count ::"+uniquecount);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//		try {
+//			Object row[] =(Object [])actorrepo.showActorsAggregateDate();
+//			System.out.println("Actors Count   ::"+row[0]);
+//			System.out.println("Actors max fee ::"+row[1]);
+//			System.out.println("Actors min fee ::"+row[2]);
+//			System.out.println("Actors avg fee ::"+row[3]);
+//			System.out.println("Actors sum of fee ::"+row[3]);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//		try {
+//			int countup=actorrepo.hikeActorFeeByCategory("Action", 5.0);
+//			System.out.println("Number of actors that are upadated "+countup);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//		try {
+//			int countdel=actorrepo.removeActorsByAddr("Bhopal");
+//			System.out.println("deleted actors count ::"+countdel);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		
+		
 		try {
-			actorrepo.showActorsByFeeRange(1000.0, 1500.0).forEach(System.out::println);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("===========================================================================");
-		try {
-			actorrepo.showActorsByCategory("Drama", "Horror", "comdey").forEach(System.out::println);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("===========================================================================");
-
-		try {
-			actorrepo.showActorsByAddr("Mumbai").forEach(row -> System.out.println(Arrays.toString(row)));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("===========================================================================");
-
-		try {
-			actorrepo.showActorsNameByFeeRange(1000, 1500).forEach(System.out::println);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("=========================");
-
-		try {
-			Optional<Actor> opt = actorrepo.showActorByMobileNumber(9876543237L);
-			if (opt.isEmpty()) {
-				System.out.println("Actor not found");
-			} else {
-				System.out.println(opt.get());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("=========================");
-		try {
-			Object obj = actorrepo.showActorDataByMobileNumber(9876543237L);
-
-			Object row[] = (Object[]) obj;
-
-			System.out.println("Actor id " + row[0]);
-			System.out.println("Actor name " + row[1]);
-			System.out.println("Actor contact number " + row[2]);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("=========================");
-
-		try {
-			String name = actorrepo.showActorNameByMobileNumber(9876543237L);
-			System.out.println("Actor name " + name);
+			LocalDateTime ldate =actorrepo.showsystemDateTime();
+			System.out.println("System date is ::"+ldate);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
 
 }
