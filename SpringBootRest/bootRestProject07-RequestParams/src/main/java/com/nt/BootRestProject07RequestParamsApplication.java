4acd2902dc4d50515d5e82a7269b805035425cd0
divyasri mygapula@LAPTOP-9JOAST2M <dineshmygapula.mca@gmail.com>
2025-07-12 15:59:10 +0530

    Springboot rest json and xml

---
+++ SpringBootRest/bootRestProject07-RequestParams/src/main/java/com/nt/BootRestProject07RequestParamsApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestProject07RequestParamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestProject07RequestParamsApplication.class, args);
+	}
+
+}
